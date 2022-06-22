b270f98728f9dd396a6566753d7da52a982c6308
masete <nicholasmasete72@gmail.com>
2022-06-23 00:17:22 +0300

    Added for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.scss';
-import { Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 
 function App() {
   return (
-    <Router>
+    <Routes>
       <Route path="/" element={<Layout />} />
-    </Router>
+    </Routes>
    
   );
 }
