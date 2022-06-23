aab826fc4298669f6027430b29afa845034fc36d
masete <nicholasmasete72@gmail.com>
2022-06-23 18:54:59 +0300

    added firebase configs

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,75 +1,29 @@
-import { useRef } from 'react';
-import { auth, storage, db } from '../../firebase';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { addDoc } from 'firebase/firestore';
-import { collection } from 'firebase/firestore/lite';
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Home from "./home";
+import Login from '../Login';
 
+const Dashboard = () => {
 
-const Home = () => {
-    const form = useRef();
+    const [user, setUser] = useState(null);
+    const auth = getAuth();
 
-    const submitPortfolio = (e) => {
-        e.preventDefault();
-        const name = form.current[0]?.value;
-        const description = form.current[1]?.value;
-        const url = form.current[2]?.value;
-        const image = form.current[3]?.files[0];
-
-        const storageRef = ref(storage, `portfolio/${image.name}`);
-
-        uploadBytes(storageRef, image).then(
-            (snapshot) => {
-                getDownloadURL(snapshot.ref).then((downloadUrl) => {
-                    savePortfolio({
-                        name,
-                        description,
-                        url,
-                        image: downloadUrl
-                    })
-                }, (error) => {
-                    console.log(error);
-                    savePortfolio({
-                        name,
-                        description,
-                        url,
-                        image: null
-                    })
-                })
-            }, (error) => {
-                console.log(error);
-                savePortfolio({
-                    name,
-                    description,
-                    url,
-                    image: null
-                })
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user) {
+                setUser(user);
+            } else {
+                setUser(null);
             }
-        )
-    }
+        })
+    }, []);
 
-    const savePortfolio = async (portfolio) => {
-        console.log(portfolio);
-        try {
-            await addDoc(collection(db, 'portfolio'), portfolio);
-            window.location.reload(false);
-        } catch (error) {
-            alert('Failed to add portfolio');
-        }
-    }
 
     return (
-        <div className="dashboard">
-
-            <form ref={form} onSubmit={submitPortfolio}>
-                <p><input type="text" placeholder="Name" /></p>
-                <p><textarea placeholder="Description" /></p>
-                <p><input type="text" placeholder="Url" /></p>
-                <p><input type="file" placeholder="Image" /></p>
-                <button type="submit">Submit</button>
-                <button onClick={() => auth.signOut()}>Sign out</button>
-            </form>
-        </div>
+       <div>
+           {user ? <Home /> : <Login />}
+       </div>
     )
 }
 
-export default Home;
+export default Dashboard;
